a0ca575730ac02767c7722a5e2ee05c4e386904f
Gustavo Sanchez O <gustavo_3141240147@utd.edu.mx>
2026-09-20 20:40:53 -0600

    fix to import

--- app/models/usuario.py
+++ app/models/usuario.py
@@ -1,11 +1,14 @@
 from datetime import datetime, timezone
+from typing import TYPE_CHECKING, List
 
 from sqlalchemy import Boolean, DateTime, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database.base import Base
-from sqlalchemy.orm import relationship
-from typing import List
+
+if TYPE_CHECKING:
+    from app.models.proyecto import Proyecto
+
 
 
 class Usuario(Base):
